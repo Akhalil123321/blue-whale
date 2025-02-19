3f929d1303bcf9968773f5bd277f7c030f1c69ea
Ahmed Khalil <ahmed.mohamed.khalil1998@gamail.com>
2025-02-18 16:26:59 -0800

    .

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import logo from '../../Images/BLUE WHALE.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          COMMING SOON...
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
