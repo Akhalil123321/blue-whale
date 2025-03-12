e9079dece5a4f8d99a1c762a45fbc9ff0e3c418a
Ahmed Khalil <ahmed.mohamed.khalil1998@gamail.com>
2025-03-13 00:43:35 +0400

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import logo from '../../Images/BLUE WHALE.png'
 import './Hero.css'
 
 const Hero = () => {
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>
-              COMMING SOON...
-            </h1>
-          </header>
+        <div className="hero-cont">
         </div>
       );
 }
